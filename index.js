7c49daf25e74c6e384b2296e78c2b67ee68f9dcc
Reyan <reyanjohnnarvaja@gmail.com>
2024-01-23 01:20:09 +0800

    added main nav and footer's sitemap

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const ejs = require('ejs');
 
 const port = 3000;
 
 app.set('view engine', 'ejs')
+app.use(express.static('./public'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('pages/home')
+    res.render('./pages/home.ejs')
 })
 
 app.listen(port, () =>{
